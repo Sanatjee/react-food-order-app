d4491ef1d3699a72b52441e46814bc7ba508a427
Sanat Gawade <sanat.leo9@gmail.com>
2022-01-18 19:28:56 +0530

    Cart Provider Added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './CartContext'
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const updatedItem = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        return {
+            items: updatedItem,
+            totalAmount: updatedTotalAmount
+        }
+    } else if (action.type === 'REMOVE_ITEM') {
+
+    }
+    return defaultCartState;
+}
+
 const Cartprovider = (props) => {
 
-    const addItemToCartHandler = (item) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({
+            type: 'ADD_ITEM',
+            item: item
+        });
     }
 
     const removeItemFromCartHandler = (id) => {
-
+        dispatchCartAction({
+            type: 'REMOVE_ITEM',
+            id: id
+        });
     }
     const cartContext = [{
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     }];
